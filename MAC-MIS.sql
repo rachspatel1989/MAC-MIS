6db8073600507931571d3119e5748db446961be5
rachspatel1989 <30328545+rachspatel1989@users.noreply.github.com>
2017-08-25 17:47:02 -0400

    Add files via upload

--- MAC-MIS.sql
+++ MAC-MIS.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2017-07-30 14:50:25
+Date: 2017-08-25 17:43:50
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -31,11 +31,14 @@ CREATE TABLE `address_details` (
   `mailing_country` varchar(255) DEFAULT NULL,
   `student_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`address_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of address_details
 -- ----------------------------
+INSERT INTO `address_details` VALUES ('1', 'Howard', 'Windsor', 'Ontario', 'Canada', 'Rosedale', 'Vadodara', 'Gujarat', 'India', '1');
+INSERT INTO `address_details` VALUES ('2', 'Randolph', 'Windsor', 'Ontario', 'Canada', 'Vasna', 'Ahemdabad', 'Gujarat', 'India', '2');
+INSERT INTO `address_details` VALUES ('3', 'Randolph', 'Windsor', 'Ontario', 'Canada', '47 DEERFIELD DRIVE, 207', 'OTTAWA', 'Ontario', 'Canada', '3');
 
 -- ----------------------------
 -- Table structure for `certification_details`
@@ -47,11 +50,15 @@ CREATE TABLE `certification_details` (
   `certi_body` varchar(255) DEFAULT NULL,
   `degree_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`certi_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of certification_details
 -- ----------------------------
+INSERT INTO `certification_details` VALUES ('1', 'CCNA,SQL', 'Cisco,Oracle', '1');
+INSERT INTO `certification_details` VALUES ('2', 'CCNA,Sql', 'Cisco,Oracle', '2');
+INSERT INTO `certification_details` VALUES ('3', 'Java', 'Oracle', '3');
+INSERT INTO `certification_details` VALUES ('4', 'CCNA', 'Cisco', '4');
 
 -- ----------------------------
 -- Table structure for `company_details`
@@ -59,6 +66,7 @@ CREATE TABLE `certification_details` (
 DROP TABLE IF EXISTS `company_details`;
 CREATE TABLE `company_details` (
   `company_id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(255) DEFAULT NULL,
   `company_address` varchar(255) DEFAULT NULL,
   `company_city` varchar(255) DEFAULT 'Windsor',
   `company_province` varchar(255) DEFAULT 'Ontario',
@@ -67,16 +75,18 @@ CREATE TABLE `company_details` (
   `company_contact_fname` varchar(255) DEFAULT NULL,
   `company_contact_lname` varchar(255) DEFAULT NULL,
   `company_contact_position` varchar(255) DEFAULT NULL,
-  `company_phone` varchar(255) DEFAULT NULL,
+  `company_phone` varchar(255) NOT NULL,
   `company_email` varchar(255) DEFAULT NULL,
   `company_website` varchar(255) DEFAULT NULL,
   `company_notes` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`company_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of company_details
 -- ----------------------------
+INSERT INTO `company_details` VALUES ('1', 'University Of Windsor', 'Sunset Avenue', 'Windsor', 'Ontario', 'N7E1$G', 'Canada', 'Dr. Stephanos  ', 'Mavromoustakos', 'Project Manager', '245-577-5678', 'stephano@uwindsor.ca', 'http://www.uwindsor.ca/', 'Test');
+INSERT INTO `company_details` VALUES ('2', 'BlackBerry Limited', 'Waterloo', 'Toronto', 'Ontario', 'K2J6D8', 'Canada', 'James', 'Stan', 'HR Manager', '566-668-6778', 'james.stan@gmail.com', 'https://ca.blackberry.com', 'Test');
 
 -- ----------------------------
 -- Table structure for `education_details`
@@ -89,17 +99,20 @@ CREATE TABLE `education_details` (
   `degree_cgpa` varchar(255) DEFAULT NULL,
   `degree_university` varchar(255) DEFAULT NULL,
   `degree_university_location` varchar(255) DEFAULT NULL,
-  `degree_start_month` varchar(255) DEFAULT NULL,
-  `degree_end_month` varchar(255) DEFAULT NULL,
-  `degree_start_year` varchar(255) DEFAULT NULL,
-  `degree_end_year` varchar(255) DEFAULT NULL,
+  `degree_start_month_year` varchar(255) DEFAULT NULL,
+  `degree_end_month_year` varchar(255) DEFAULT NULL,
+  `degree_status` varchar(255) DEFAULT NULL,
   `student_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`degree_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of education_details
 -- ----------------------------
+INSERT INTO `education_details` VALUES ('1', 'Undergraduate', 'Bachelor of Engineering', '8.91', 'Gujarat Technological University', 'India', '2010-08', '2013-06', 'Previous', '1');
+INSERT INTO `education_details` VALUES ('2', 'Graduate', 'Masters of Applied Computing', '8.55', 'University of Windsor', 'Canada', '2017-01', '2018-04', 'Current', '1');
+INSERT INTO `education_details` VALUES ('3', 'Undergraduate', 'Bachelor of Engineering', '8.55', 'Gujarat Technological University', 'India', '2013-08', '2015-06', 'Previous', '2');
+INSERT INTO `education_details` VALUES ('4', 'Graduate', 'Masters of Applied Computing', '8.20', 'University of Windsor', 'Canada', '2016-09', '2017-04', 'Current', '3');
 
 -- ----------------------------
 -- Table structure for `internship_type`
@@ -109,11 +122,14 @@ CREATE TABLE `internship_type` (
   `internship_type_id` int(11) NOT NULL AUTO_INCREMENT,
   `internship_type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`internship_type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of internship_type
 -- ----------------------------
+INSERT INTO `internship_type` VALUES ('1', 'Industry');
+INSERT INTO `internship_type` VALUES ('2', 'MAC Project');
+INSERT INTO `internship_type` VALUES ('3', 'Other');
 
 -- ----------------------------
 -- Table structure for `job_details`
@@ -121,19 +137,21 @@ CREATE TABLE `internship_type` (
 DROP TABLE IF EXISTS `job_details`;
 CREATE TABLE `job_details` (
   `job_id` int(11) NOT NULL AUTO_INCREMENT,
-  `job_group_id` int(11) DEFAULT NULL,
-  `job_company_id` int(11) DEFAULT NULL,
-  `job_internship_type_id` int(11) DEFAULT NULL,
+  `job_group_id` varchar(11) DEFAULT NULL,
+  `job_company_id` varchar(11) DEFAULT NULL,
+  `job_internship_type_id` varchar(11) DEFAULT NULL,
   `job_position` varchar(255) DEFAULT NULL,
   `job_description` varchar(255) DEFAULT NULL,
   `job_salary` decimal(10,0) DEFAULT NULL,
   `job_status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`job_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of job_details
 -- ----------------------------
+INSERT INTO `job_details` VALUES ('1', '1', '1', '2', 'Web Developer', 'Develop the MAC Management Information System.', '0', 'Open');
+INSERT INTO `job_details` VALUES ('2', '5', '2', '1', 'Network Administrator', 'System Administration', '40000', 'Open');
 
 -- ----------------------------
 -- Table structure for `job_groups`
@@ -143,11 +161,20 @@ CREATE TABLE `job_groups` (
   `job_group_id` int(11) NOT NULL AUTO_INCREMENT,
   `job_group_description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`job_group_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of job_groups
 -- ----------------------------
+INSERT INTO `job_groups` VALUES ('1', 'Web Development');
+INSERT INTO `job_groups` VALUES ('2', 'Mobile Development');
+INSERT INTO `job_groups` VALUES ('3', 'System Development');
+INSERT INTO `job_groups` VALUES ('4', 'Technical Support');
+INSERT INTO `job_groups` VALUES ('5', 'Networking');
+INSERT INTO `job_groups` VALUES ('6', 'Data Analysis');
+INSERT INTO `job_groups` VALUES ('7', 'Testing');
+INSERT INTO `job_groups` VALUES ('8', 'Security');
+INSERT INTO `job_groups` VALUES ('9', 'Data Management');
 
 -- ----------------------------
 -- Table structure for `job_requirements`
@@ -158,11 +185,13 @@ CREATE TABLE `job_requirements` (
   `job_req_desc` varchar(255) DEFAULT NULL,
   `job_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`job_req_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of job_requirements
 -- ----------------------------
+INSERT INTO `job_requirements` VALUES ('1', 'PHP/MySQL', '1');
+INSERT INTO `job_requirements` VALUES ('2', 'TCP/IP Network', '2');
 
 -- ----------------------------
 -- Table structure for `job_responsibilities`
@@ -173,11 +202,13 @@ CREATE TABLE `job_responsibilities` (
   `job_resp_desc` varchar(255) DEFAULT NULL,
   `job_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`job_resp_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of job_responsibilities
 -- ----------------------------
+INSERT INTO `job_responsibilities` VALUES ('1', 'Develop,Design,Analyze', '1');
+INSERT INTO `job_responsibilities` VALUES ('2', 'Maintain', '2');
 
 -- ----------------------------
 -- Table structure for `semester_registered`
@@ -189,11 +220,14 @@ CREATE TABLE `semester_registered` (
   `semester_year` varchar(255) DEFAULT NULL,
   `student_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`semester_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of semester_registered
 -- ----------------------------
+INSERT INTO `semester_registered` VALUES ('1', 'Winter', '2017', '1');
+INSERT INTO `semester_registered` VALUES ('2', 'Fall', '2016', '2');
+INSERT INTO `semester_registered` VALUES ('3', 'Winter', '2016', '3');
 
 -- ----------------------------
 -- Table structure for `student_details`
@@ -201,20 +235,25 @@ CREATE TABLE `semester_registered` (
 DROP TABLE IF EXISTS `student_details`;
 CREATE TABLE `student_details` (
   `student_id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_number` varchar(255) DEFAULT NULL,
   `student_fname` varchar(255) DEFAULT NULL,
   `student_mname` varchar(255) DEFAULT NULL,
   `student_lname` varchar(255) DEFAULT NULL,
+  `student_uname` varchar(255) DEFAULT NULL,
   `student_email` varchar(255) DEFAULT NULL,
   `student_phone` varchar(255) DEFAULT NULL,
   `student_gender` varchar(255) DEFAULT NULL,
   `student_status` varchar(255) DEFAULT NULL,
   `student_current_past` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`student_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of student_details
 -- ----------------------------
+INSERT INTO `student_details` VALUES ('1', '104704646', 'Rachana', 'Kamleshbhai', 'Patel', 'rachspatel1989', 'rachspatel1989@gmail.com', '255-566-5678', 'Female', 'International', 'Current');
+INSERT INTO `student_details` VALUES ('2', '104656282', 'Bansari', 'Pragnesh', 'Shah', 'bansushah', 'shahbansu94@gmail.com', '344-678-6788', 'Female', 'International', 'Current');
+INSERT INTO `student_details` VALUES ('3', '104705681', 'Charmi', 'Nilesh', 'Shah', 'charmishah', 'charmishah0829@gmail.com', '345-678-3456', 'Female', 'International', 'Current');
 
 -- ----------------------------
 -- Table structure for `student_internship_details`
@@ -224,12 +263,15 @@ CREATE TABLE `student_internship_details` (
   `stu_internship_id` int(11) NOT NULL AUTO_INCREMENT,
   `job_id` int(11) DEFAULT NULL,
   `student_id` int(11) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`stu_internship_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of student_internship_details
 -- ----------------------------
+INSERT INTO `student_internship_details` VALUES ('1', '1', '2', 'Unpaid');
+INSERT INTO `student_internship_details` VALUES ('2', '2', '1', 'Paid');
 
 -- ----------------------------
 -- Table structure for `user`
@@ -244,11 +286,16 @@ CREATE TABLE `user` (
   `user_password` varchar(255) DEFAULT NULL,
   `user_type_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
+INSERT INTO `user` VALUES ('1', 'Admin', 'Test', 'admin123', 'admin@gmail.com', 'admin123', '1');
+INSERT INTO `user` VALUES ('2', 'Faculty', 'Test', 'faculty', 'faculty@gmail.com', 'faculty123', '2');
+INSERT INTO `user` VALUES ('3', 'Rachana', 'Patel', 'rachspatel1989', 'rachspatel1989@gmail.com', 'rachs123', '3');
+INSERT INTO `user` VALUES ('4', 'Bansari', 'Shah', 'bansushah', 'shahbansu94@gmail.com', 'bansu123', '3');
+INSERT INTO `user` VALUES ('5', 'Charmi', 'Shah', 'charmishah', 'charmishah0829@gmail.com', 'charmi123', '3');
 
 -- ----------------------------
 -- Table structure for `user_type`
@@ -258,8 +305,11 @@ CREATE TABLE `user_type` (
   `user_type_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`user_type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_type
 -- ----------------------------
+INSERT INTO `user_type` VALUES ('1', 'staff');
+INSERT INTO `user_type` VALUES ('2', 'faculty');
+INSERT INTO `user_type` VALUES ('3', 'student');
---
+++ Readme.txt
@@ -0,0 +1,23 @@
+Project Title: MAC-Management Information System
+
+Project Hosted Link:http://mac-mis.itechnologygroups.com/
+Project Location(URL):	https://github.com/rahulgarg07/MAC-MIS-Project-60-699-
+
+Login Credentials:
+Staff Login: username:admin@gmail.com password:admin123
+Faculty Login: username:faculty@gmail.com password:faculty123
+Student Login: username:rachspatel1989@gmail.com password:rachs123
+
+Group Members Name:
+
+1. Rachana Patel (104704646 ) 
+2. Charmi Shah (104705681)
+3. Bansari Shah (104656282)
+4. Rahul Patel (104676161)
+
+
+Technologies Used: PHP, HTML, CSS, MYSQL, JQuery, JavaScript.
+             We used PHP for web development,HTML for web page creation with the use of CSS and Javascript.For giving style to html pages we use Cascading Style Sheets with HTML.With the use of MYSQL we create a database. 
+
+Purpose of the site: 
+             The purpose of the MAC-MIS is to manage MAC students’ records, from registration to graduation to alumni.There are different account for the users such as Student account, Staff account, Faculty account etc. Every user can maintain their account. 
